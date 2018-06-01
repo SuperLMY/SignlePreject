0b85c9faf4b2610d9c3c8559b47a918f7f954d76
李明洋 <792923342@qq.com>
2018-06-01 11:30:59 +0800

    new change

---
+++ AttendanceWork/AttendanceWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttendanceWork
+//
+//  Created by 李明洋 on 2018/6/1.
+//  Copyright © 2018年 李明洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
